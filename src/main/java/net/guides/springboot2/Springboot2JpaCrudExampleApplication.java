2e6c2d4e4cc285d0be0157b84cb7a686c91b2e70
Honggo Yeo <honggo@websatmedia.com>
2022-02-17 08:47:52 +0800

    Test Branch 1 [2022-02-17]

--- src/main/java/net/guides/springboot2/Springboot2JpaCrudExampleApplication.java
+++ src/main/java/net/guides/springboot2/Springboot2JpaCrudExampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springboot2JpaCrudExampleApplication {
-
+	// To test branching
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2JpaCrudExampleApplication.class, args);
 	}
